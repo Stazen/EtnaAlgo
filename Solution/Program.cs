89de15853b2dd7033f0480c0679e02d4901c7569
Julien TOSTI <DSI_JTO@maty.local>
2022-02-10 11:34:06 +0100

    Ajout de Stormtroopers

--- Solution/Program.cs
+++ Solution/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Solution
 {
@@ -6,19 +7,46 @@ namespace Solution
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(playset("abcde"));
-            Console.WriteLine(playset("abbcdde"));
-            Console.WriteLine(playset("abcdeab"));
+            Console.WriteLine(Stormtroopers(new List<int>{ 1, 2, 3, 4, 5 }));
+            Console.WriteLine(Stormtroopers(new List<int>{ 1, 2, 2, 3, 4, 4, 5 }));
         }
+
+        public static List<int> Stormtroopers(List<int> numbers)
+        {
+            int dynamicSize = numbers.Count;
+           
+            for (int i = 0; i < dynamicSize; i++)
+            {
+                for (int y = i+1; y < dynamicSize; y++)
+                {
+                   if (numbers[i] == numbers[y])
+                    {
+                        int del = numbers[y];
+                        for (int z = 0; z < dynamicSize; z++)
+                        {
+                            if (numbers[z] == del)
+                            {
+                                numbers.RemoveAt(z);
+                                dynamicSize = numbers.Count;
+                                z = 0;
+                            }
+                        }
+                        i = 0;                       
+                    }
+                }
+            }
+            return numbers;
+        }
+
+
 
         public static bool playset(string s) 
         {
             int count = 1;
             foreach (char c in s)
             {
-                char charSelected = c;
                 for (int i = count; i < s.Length; i++)
-                    if (s[i] == charSelected)
+                    if (s[i] == c)
                     {
                         return true;
                     }
