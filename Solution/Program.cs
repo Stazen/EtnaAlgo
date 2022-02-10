ed8185f0d2556db75e4cdce3e822afe0d3e7c9a7
Julien TOSTI <DSI_JTO@maty.local>
2022-02-10 10:36:24 +0100

    Ajout du playset

---
+++ Solution/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(playset("abcde"));
+            Console.WriteLine(playset("abbcdde"));
+            Console.WriteLine(playset("abcdeab"));
+        }
+
+        public static bool playset(string s) 
+        {
+            int count = 1;
+            foreach (char c in s)
+            {
+                char charSelected = c;
+                for (int i = count; i < s.Length; i++)
+                    if (s[i] == charSelected)
+                    {
+                        return true;
+                    }
+                count++;
+            }
+            return false;
+        }
+    }
+}
